d796ef3172a5e1e49d4f89f69678a56cd5726ed3
Shahzada Ali Hassan <59549481+HassanAMZ@users.noreply.github.com>
2022-09-07 17:32:50 +0500

    Updated GTM Script Loading Stategry

--- pages/_app.js
+++ pages/_app.js
@@ -29,7 +29,7 @@ export default function App({ Component, pageProps }) {
       {/* Google Tag Manager - Global base code */}
       <Script
         id="gtm-script"
-        strategy="afterInteractive"
+        strategy="lazyOnload"
         dangerouslySetInnerHTML={{
           __html: `
           (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
