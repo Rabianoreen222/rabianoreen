076365e447e7ac3d5e22ecc19686a439029030c6
Shahzada Ali Hassan <59549481+HassanAMZ@users.noreply.github.com>
2022-09-07 17:30:16 +0500

    Updated GTM

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,24 @@
 import '@/css/tailwind.css'
 import '@/css/prism.css'
 import 'katex/dist/katex.css'
-
 import '@fontsource/inter/variable-full.css'
-
 import { ThemeProvider } from 'next-themes'
 import Head from 'next/head'
-
 import siteMetadata from '@/data/siteMetadata'
 import Analytics from '@/components/analytics'
 import LayoutWrapper from '@/components/LayoutWrapper'
 import { ClientReload } from '@/components/ClientReload'
+import Script from 'next/script'
 
 const isDevelopment = process.env.NODE_ENV === 'development'
 const isSocket = process.env.SOCKET
+let GTM_Tracking_ID = siteMetadata.analytics.googleTagManagerID || ''
+const isProduction = process.env.NODE_ENV === 'production'
+if (isProduction) {
+  GTM_Tracking_ID = siteMetadata.analytics.googleTagManagerID || ''
+} else {
+  GTM_Tracking_ID = 'GTM-XXXXXXX'
+}
 
 export default function App({ Component, pageProps }) {
   return (
@@ -21,6 +26,20 @@ export default function App({ Component, pageProps }) {
       <Head>
         <meta content="width=device-width, initial-scale=1" name="viewport" />
       </Head>
+      {/* Google Tag Manager - Global base code */}
+      <Script
+        id="gtm-script"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer', '${GTM_Tracking_ID}');
+        `,
+        }}
+      />
       {isDevelopment && isSocket && <ClientReload />}
       <Analytics />
       <LayoutWrapper>
--- pages/_document.js
+++ pages/_document.js
@@ -4,11 +4,6 @@ class MyDocument extends Document {
     return (
       <Html lang="en" className="scroll-smooth">
         <Head>
-<Script strategy="afterInteractive" dangerouslySetInnerHTML={{ __html:'(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-56PN3SS');`}}></Script>
           <link rel="apple-touch-icon" sizes="76x76" href="/static/favicons/apple-touch-icon.png" />
           <link
             rel="icon"
@@ -29,10 +24,9 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
           <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
         </Head>
+
         <body className="bg-white text-black antialiased dark:bg-gray-900 dark:text-white">
- <noscript dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-56PN3SS"
-height="0" width="0" style="display:none;visibility:hidden"></iframe>`}}></noscript>
-         <Main />
+          <Main />
           <NextScript />
         </body>
       </Html>
